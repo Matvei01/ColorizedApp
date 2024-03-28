1f9ef4756e4b671c99bdde2681c16fe3226e240f
Matvei <khlestov.matvei@mail.ru>
2024-03-28 20:11:55 +0500

    Created base project, removed storyboard references

---
+++ ColorizedApp/ColorizedApp/App/SceneDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  SceneDelegate.swift
+//  ColorizedApp
+//
+//  Created by Matvei Khlestov on 28.03.2024.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene,
+               willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {
+        
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        let window = UIWindow(windowScene: windowScene)
+        let viewController = ColorizedViewController()
+        window.rootViewController = viewController
+        self.window = window
+        window.makeKeyAndVisible()
+    }
+}
+
---
+++ ColorizedApp/ColorizedApp/Controllers/ColorizedViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedViewController.swift
+//  ColorizedApp
+//
+//  Created by Matvei Khlestov on 28.03.2024.
+//
+
+import UIKit
+
+final class ColorizedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
+
